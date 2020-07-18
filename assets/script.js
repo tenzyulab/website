f60b55c2ac22087c7f03e3ff188fddf422b09556
fe-amazu <tenzyu.on@gmail.com>
2020-07-19 04:21:57 +0900

    fixed doCopy

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,18 @@
-function doCopy(txt) {
-    var ta = document.createElement("textarea");
-    document.getElementsByTagName("body")[0].appendChild(ta);
-    ta.value = txt;
-    ta.select();
-    var ret = document.execCommand("copy");
-    ta.parentNode.removeChild(ta);
-    alert("Copied!");
+function doCopyDiscordId() {
+  var ta = document.createElement("textarea");
+  ta.value = "..........#9999";
+  var bodyElm = document.getElementsByTagName("body")[0];
+  bodyElm.appendChild(ta);
+  ta.select();
+  var result = document.execCommand("copy");
+  bodyElm.removeChild(ta);
+  if (result) {
+    var area = document.getElementById("clip-discord");
+    area.innerText = "Copied!";
+    setTimeout(reset, 2500)
+  }
+
+  function reset() {
+    area.innerText = "click to copy my ID."
+  }
 }
