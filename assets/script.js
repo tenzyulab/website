e4750e1119a6cb652bcbb3fd776588aee6420653
fe-amazu <tenzyu.on@gmail.com>
2020-07-23 00:00:32 +0900

    remove :hover on smartphones

--- assets/script.js
+++ assets/script.js
@@ -17,3 +17,24 @@ function doCopyDiscordId() {
     area.innerText = "Click to copy my ID."
   }
 }
+
+var touch = 'ontouchstart' in document.documentElement ||
+  navigator.maxTouchPoints > 0 ||
+  navigator.msMaxTouchPoints > 0;
+
+if (touch) {
+  try {
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
