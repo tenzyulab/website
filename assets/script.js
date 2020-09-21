ca444480a7a54fac0536e04307eb6a520357d90a
fe-amazu <tenzyu.on@gmail.com>
2020-09-21 18:12:47 +0900

    fixed my discord tag

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,6 @@
 function doCopyDiscordId() {
   var ta = document.createElement("textarea");
-  ta.value = "tenzyu#9999";
+  ta.value = "tenzyu#8219";
   var bodyElm = document.getElementsByTagName("body")[0];
   bodyElm.appendChild(ta);
   ta.select();
